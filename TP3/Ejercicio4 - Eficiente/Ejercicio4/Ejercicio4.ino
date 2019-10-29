c017b56354070a7513d2ebfe0597462e0ca9aab9
Julieta <july1995bolt@gmail.com>
2019-10-29 16:44:23 -0300

    Utilizacion de variables contadores- Cambio de indice para que indique la secuencia

--- TP3/Ejercicio4 - Eficiente/Ejercicio4/Ejercicio4.ino
+++ TP3/Ejercicio4 - Eficiente/Ejercicio4/Ejercicio4.ino
@@ -13,38 +13,43 @@ SemaphoreHandle_t s_tarea1;
 SemaphoreHandle_t s_tarea2;
 SemaphoreHandle_t s_tarea3;
 
-static SemaphoreHandle_t sc_tarea2 = xSemaphoreCreateCounting(2, 0);
-static SemaphoreHandle_t sc_tarea3 = xSemaphoreCreateCounting(3, 0);
+static int sc_tarea2 = 0; //hasta 2
+static int sc_tarea3 = 0; //hasta 3
 // -------------------------- FUNCIONES ---------------------------------- //
 
 
 void tarea1(int i){
     while(1){
-    xSemaphoreTake(s_tarea1, portMAX_DELAY);
+        xSemaphoreTake(s_tarea1, portMAX_DELAY);
         Serial.println(i); //le sumo 1 para que escriba de 1 a 3    
         vTaskDelay(50);
         if (input == 'a')
             xSemaphoreGive(s_tarea3);
         else if (input == 'b')
-            xSemaphoreGive(sc_tarea2);
+            xSemaphoreGive(s_tarea2);
         else if (input == 'c')
             xSemaphoreGive(s_tarea2);
     }
 }
 void tarea2(int i){
     while(1){
-        xSemaphoreTake(sc_tarea2, portMAX_DELAY);
+        xSemaphoreTake(s_tarea2, portMAX_DELAY);
         Serial.println(i); //le sumo 1 para que escriba de 1 a 3    
         vTaskDelay(50);
         if (input == 'a')
             xSemaphoreGive(s_tarea1);
         else if (input == 'b'){
-            xSemaphoreGive(sc_tarea2);
-            if(xSemaphoreGetCount(sc_tarea2)==0) 
+            
+            if(sc_tarea2 == 1){ //si llego a contador maximo
                 xSemaphoreGive(s_tarea3);
+                sc_tarea2 = 0;
+            }else{
+                sc_tarea2++;
+                xSemaphoreGive(s_tarea2);
+            }
         }
         else if (input == 'c')
-            xSemaphoreGive(sc_tarea3);
+            xSemaphoreGive(s_tarea3);
 
     }
 }
@@ -58,10 +63,13 @@ void tarea3(int i){
         else if (input == 'b')
             xSemaphoreGive(s_tarea1);
         else if (input == 'c'){
-            if(xSemaphoreGetCount(sc_tarea3)==0)
-                xSemaphoreGive(sc_tarea3);
-            else
+            if(sc_tarea3 == 2){ //si llego a contador maximo
                 xSemaphoreGive(s_tarea1);
+                sc_tarea3 = 0;
+            }else{
+                sc_tarea3++;
+                xSemaphoreGive(s_tarea3);
+            }
         }
     }
 }
@@ -77,7 +85,7 @@ void setup(){
         char input = Serial.read(); //leo lo que se ingresa desde teclado
     }*/
     delay(1000);
-    input = 'a';
+    input = 'c';
 
     s_tarea1 = xSemaphoreCreateBinary();
     s_tarea2 = xSemaphoreCreateBinary();
@@ -86,14 +94,14 @@ void setup(){
     if(input == 'a'){
         xSemaphoreGive(s_tarea1);
     }else if (input == 'b')
-            xSemaphoreGive(sc_tarea2);
+            xSemaphoreGive(s_tarea2);
         else if (input == 'c')
-            xSemaphoreGive(sc_tarea3);
+            xSemaphoreGive(s_tarea3);
 
     // tareas que van a estar periodicamente haciendo interrupciones de sw
-    xTaskCreate(tarea1, "Tarea 1", 128, 0, 1, NULL);
-    xTaskCreate(tarea2, "Tarea 2", 128, 1, 1, NULL);
-    xTaskCreate(tarea3, "Tarea 3 ", 128, 2, 1, NULL);
+    xTaskCreate(tarea1, "Tarea 1", 128, 1, 1, NULL);
+    xTaskCreate(tarea2, "Tarea 2", 128, 2, 1, NULL);
+    xTaskCreate(tarea3, "Tarea 3 ", 128, 3, 1, NULL);
 
     vTaskStartScheduler();
 
