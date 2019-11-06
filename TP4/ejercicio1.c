eed630f9ecfcaa112cfdbd2ade22a536f6157161
Julieta <july1995bolt@gmail.com>
2019-11-06 15:46:54 -0300

    Ejercicio 1 funcionando

--- TP4/ejercicio1.c
+++ TP4/ejercicio1.c
@@ -1,44 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/time.h>
 
+#define N 2
+#define ITERACIONES 1000
 
+long int  promedios[N];
+int ids[N];
 
-#define N 2 // numero de hilos
-#define ITERACIONES 1000 // cantidad de veces que se ejecutan
-
-long long promedios[N]; // array para guardar los promedios de cada hilo
-
-void *procesar(void *arg){ // procesos de los hilos
-
-    // me guardo el tiempo del principio
+void *procesar(void *arg){
     int i;
-    for (i = 0; i < ITERACIONES; i++) { // para que se ejecuten 1000 veces
-       
+    long int tiempo = 0;
+    struct timeval act, ant;
 
-
+    for (i=0; i<ITERACIONES; i++){
+        //tomo tiempo ant en ms 
+        gettimeofday(&ant,NULL);
+        usleep(10); //delay de 10ms
+        //tomo tiempo act en ms
+        gettimeofday(&act,NULL);
+//      printf("%d \n",act.tv_sec);
+        tiempo+=(act.tv_usec-ant.tv_usec)-10;
+    //  printf("%ld \n ",tiempo);
     }
-    // termino de ejecutarse el hilo
-    promedios[] = tiempo / ITERACIONES; // <--- cual es la posicion??
+//  printf("cacacaca: %ld \n",(tiempo/ITERACIONES));
+//  printf("id: %d",*(int*)arg);
+    promedios[*(int*)arg]=tiempo/ITERACIONES;
+return NULL;
 }
 
-void main(){
-
-    // declaro array de hilos N=2
+int main(){
+//  printf("asdasd");
     pthread_t hilos[N];
-
-    // creo los hilos
     int i;
-    
-    for (i = 0; i < N; i++)
-        pthread_create(hilos + i, NULL, procesar, promedios + i)            
-    
-    // espero que terminen los hilos
-    for (i = 0; i < N; i++)
-        pthread_join(hilos[i], NULL)
-    
-    // imprimo promedios
-    for (i = 0; i < N; i++)
-        printf("El promedio del thread %i es %lld nanosegundos.\n", i, promedios[i]);
+//  printf("holaasd");
+    sleep(4);
+    for (i=0;i<N;i++){
+       ids[i]=i;
+       pthread_create(hilos+i,NULL,procesar,(void*)&ids[i]);
+    }
+//  printf("jdsfnkjsdf");   
+    for(i=0;i<N;i++)
+       pthread_join(hilos[i],NULL);
+    for(i=0;i<N;i++)
+       printf("El promedio del thread %i es %ld milisegundos. \n",i,promedios[i]);
 
+return (0);
 }
